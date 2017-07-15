97455502a9d96974c8d9ef0327da7007f4f237f6
Aadam <aadimator@gmail.com>
2017-07-15 10:34:26 +0500

    feat: Add RecyclerView

--- BakingApp/app/src/main/java/com/aadimator/android/bakingapp/MainActivity.java
+++ BakingApp/app/src/main/java/com/aadimator/android/bakingapp/MainActivity.java
@@ -2,8 +2,12 @@ package com.aadimator.android.bakingapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.aadimator.android.bakingapp.adapters.RecipeAdapter;
 import com.aadimator.android.bakingapp.data.model.Recipe;
 import com.aadimator.android.bakingapp.data.remote.RecipeService;
 import com.aadimator.android.bakingapp.helpers.ApiUtils;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private RecipeService mRecipeService;
+    private RecyclerView mRecyclerView;
+    private RecipeAdapter mRecipeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecipeService = ApiUtils.getRecipeService();
+        mRecipeAdapter = new RecipeAdapter(this, new ArrayList<Recipe>(0));
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        mRecyclerView.setAdapter(mRecipeAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
         loadRecipes();
     }
 
@@ -33,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<ArrayList<Recipe>> call, Response<ArrayList<Recipe>> response) {
                 if (response.isSuccessful()) {
+                    mRecipeAdapter.updateRecipes(response.body());
                     Log.d(LOG_TAG, response.body().get(0).getName());
                 } else {
                     int statusCode = response.code();
---
+++ BakingApp/app/src/main/java/com/aadimator/android/bakingapp/adapters/RecipeAdapter.java
@@ -0,0 +1,62 @@
+package com.aadimator.android.bakingapp.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.aadimator.android.bakingapp.R;
+import com.aadimator.android.bakingapp.data.model.Recipe;
+
+import java.util.List;
+
+/**
+ * Created by Aadam on 7/16/2017.
+ */
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+    private Context mContext;
+    private List<Recipe> mRecipeList;
+
+    public RecipeAdapter(Context context, List<Recipe> recipes) {
+        mContext = context;
+        mRecipeList = recipes;
+    }
+
+    @Override
+    public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_card, parent, false);
+        return new RecipeViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeViewHolder holder, int position) {
+        Recipe recipe = mRecipeList.get(position);
+        holder.recipeName.setText(recipe.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipeList.size();
+    }
+
+    public void updateRecipes(List<Recipe> recipes) {
+        mRecipeList = recipes;
+        notifyDataSetChanged();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView recipeName;
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+
+            recipeName = (TextView) itemView.findViewById(R.id.recipe_name_text_view);
+        }
+    }
+}
